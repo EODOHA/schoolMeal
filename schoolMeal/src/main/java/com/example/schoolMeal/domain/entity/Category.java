befe0e7690011899ab3fd137978ea373bf2c79a1
이재학 <ggolji123@gmail.com>
2024-11-05 11:17:30 +0900

    1. validation 의존성추가 2. 카테고리 추가 (수정예정) 3. 커뮤니티 엔티티,리포지터리,DTO,서비스,컨트롤러 작성 ( 공지사항만 )

--- schoolMeal/src/main/java/com/example/schoolMeal/domain/entity/Category.java
+++ schoolMeal/src/main/java/com/example/schoolMeal/domain/entity/Category.java
@@ -1,19 +1,19 @@
-package com.example.schoolMeal.domain.entity;
-
-import jakarta.persistence.*;
-import lombok.*;
-//import com.library.constant.Role;
-
-
-@Entity
-@Table(name = "category")
-@Getter
-@Setter
-@ToString
-public class Category {
-    @Id
-    @Column(name = "category_id")
-    private Long category_id;
-
-    private String category_name;
-}
+//package com.example.schoolMeal.domain.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+////import com.library.constant.Role;
+//
+//
+//@Entity
+//@Table(name = "category")
+//@Getter
+//@Setter
+//@ToString
+//public class Category {
+//    @Id
+//    @Column(name = "category_id")
+//    private Long category_id;
+//
+//    private String category_name;
+//}
