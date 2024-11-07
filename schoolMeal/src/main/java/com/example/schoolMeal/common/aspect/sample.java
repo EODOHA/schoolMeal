d0be9a36b9ef6398a2c6dd2867631d72d0c905a2
jeong hwajin <jonghwajin@gmail.com>
2024-11-07 10:14:14 +0900

    BaseEntity 패키지 이동(common패키지 추가 생성)

---
+++ schoolMeal/src/main/java/com/example/schoolMeal/common/aspect/sample.java
@@ -0,0 +1,6 @@
+package com.example.schoolMeal.common.aspect;
+
+//push용 빈 파일 
+public class sample {
+
+}
--- schoolMeal/src/main/java/com/example/schoolMeal/common/entity/BaseEntity.java
+++ schoolMeal/src/main/java/com/example/schoolMeal/common/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.example.schoolMeal.domain.entity;
+package com.example.schoolMeal.common.entity;
 
 import java.time.LocalDateTime;
 
@@ -11,7 +11,7 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
-@MappedSuperclass // 상속하는 엔터티에서 공통 필드로 사용가능/ 상속한 엔터티에 테이블에 자동생성
+@MappedSuperclass // 상속하는 엔터티에서 공통 필드로 사용가능, 상속한 엔터티에 테이블에 자동생성
 @EntityListeners(AuditingEntityListener.class) // Auditing 기능 활성화
 @Getter 
 @Setter
