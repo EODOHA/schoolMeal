eb8ed0140868d0ab85e91ee32a76be3727f8faff
Hwajin <jonghwajin@gmail.com>
2024-12-10 15:40:02 +0900

    IngredientInfo 라우트 에러부분 주석처리

--- schoolmeal-react/src/component/IngredientInfo/routes/IngredientInfoRoutes.js
+++ schoolmeal-react/src/component/IngredientInfo/routes/IngredientInfoRoutes.js
@@ -3,8 +3,8 @@ import { Route } from "react-router-dom";
 
 // 식재료 정보 관련 컴포넌트 임포트
 import IngredientInfoMain from '../routes/IngredientInfoMain';
-import IngredientPriceList from '../lists/IngredientPriceList';
-import ProductSafetyList from '../lists/ProductSafetyList';
+// import IngredientPriceList from '../lists/IngredientPriceList';
+// import ProductSafetyList from '../lists/ProductSafetyList';
 import HaccpInfoList from '../lists/HaccpInfoList';
 import HaccpInfoEdit from '../edits/HaccpInfoEdit';
 import HaccpInfoWrite from '../writes/HaccpInfoWrite';
@@ -16,8 +16,8 @@ import WriteFileUpload from '../writes/WriteFileUpload';
 const IngredientInfoRoutes = (
   <>
     <Route path="ingredientInfo" element={<IngredientInfoMain />} />
-    <Route path="ingredientInfo/ingredient-price" element={<IngredientPriceList />} />
-    <Route path="ingredientInfo/product-safety" element={<ProductSafetyList />} />
+    {/* <Route path="ingredientInfo/ingredient-price" element={<IngredientPriceList />} />
+    <Route path="ingredientInfo/product-safety" element={<ProductSafetyList />} /> */}
     <Route path="ingredientInfo/haccp-info" element={<HaccpInfoList />} />
 
     <Route path="ingredientInfo/haccp-info/write" element={<HaccpInfoWrite />} />
