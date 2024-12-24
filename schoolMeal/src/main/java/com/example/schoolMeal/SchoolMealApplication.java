cebf41f8ac22f0103322fd072b352943b5ad7c9f
DOHA <eoryk96@naver.com>
2024-12-24 14:22:49 +0900

    기초 설정 변경

--- schoolMeal/src/main/java/com/example/schoolMeal/SchoolMealApplication.java
+++ schoolMeal/src/main/java/com/example/schoolMeal/SchoolMealApplication.java
@@ -53,41 +53,43 @@ public class SchoolMealApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		memberRepository.save(new Member(
-				"어드민",
-				"admin", 
-				"$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW",
-				"admin@admin.com",
-				"01012345678",
-				Role.ADMIN));
-		memberRepository.save(new Member(
-				"게시판관리",
-				"boardadmin", 
-				"$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW",
-				"boardAdmin@admin.com",
-				"01075623132",
-				Role.BOARDADMIN));
-		memberRepository.save(new Member(
-				"연계회원",
-				"linkMember", 
-				"$2a$12$SIIf/Riy.LSvYwbSBtsLeuiqZMBXHA/nKWhpvIHWS5W/OBUkVo0.y",
-				"linkMember@*#%*12VVGd2.com",
-				"01044217412",
-				Role.LINKAGE));
-		memberRepository.save(new Member(
-				"일반회원",
-				"member1", 
-				"$2a$12$SIIf/Riy.LSvYwbSBtsLeuiqZMBXHA/nKWhpvIHWS5W/OBUkVo0.y",
-				"member1@*#%*12VVGd2.com",
-				"01095276514",
-				Role.MEMBER));
-		memberRepository.save(new Member(
-				"강등회원",
-				"member2", 
-				"$2a$12$SIIf/Riy.LSvYwbSBtsLeuiqZMBXHA/nKWhpvIHWS5W/OBUkVo0.y",
-				"member2@*#%*12VVGd3.com",
-				"01051244884",
-				Role.GUEST));
+		if (!memberRepository.existsByEmail("admin@admin.com")) {			
+			memberRepository.save(new Member(
+					"어드민",
+					"admin", 
+					"$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW",
+					"admin@admin.com",
+					"01012345678",
+					Role.ADMIN));
+		}
+//		memberRepository.save(new Member(
+//				"게시판관리",
+//				"boardadmin", 
+//				"$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW",
+//				"boardAdmin@admin.com",
+//				"01075623132",
+//				Role.BOARDADMIN));
+//		memberRepository.save(new Member(
+//				"연계회원",
+//				"linkMember", 
+//				"$2a$12$SIIf/Riy.LSvYwbSBtsLeuiqZMBXHA/nKWhpvIHWS5W/OBUkVo0.y",
+//				"linkMember@*#%*12VVGd2.com",
+//				"01044217412",
+//				Role.LINKAGE));
+//		memberRepository.save(new Member(
+//				"일반회원",
+//				"member1", 
+//				"$2a$12$SIIf/Riy.LSvYwbSBtsLeuiqZMBXHA/nKWhpvIHWS5W/OBUkVo0.y",
+//				"member1@*#%*12VVGd2.com",
+//				"01095276514",
+//				Role.MEMBER));
+//		memberRepository.save(new Member(
+//				"강등회원",
+//				"member2", 
+//				"$2a$12$SIIf/Riy.LSvYwbSBtsLeuiqZMBXHA/nKWhpvIHWS5W/OBUkVo0.y",
+//				"member2@*#%*12VVGd3.com",
+//				"01051244884",
+//				Role.GUEST));
 		
 		IngredientPrice product1 = new IngredientPrice();
         product1.setCategory("농산물");
